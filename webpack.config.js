13678a4e5cd6fbc36f6b64cc12be02d6e68f19f6
Vadim Kasatkin <penguin2804@gmail.com>
2021-06-17 20:04:08 +0400

    webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+  entry: "./src/main.css",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
+
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+    new CssMinimizerPlugin(),
+  ],
 };
